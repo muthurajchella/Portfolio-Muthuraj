162c259ac5f9399e5382bda111a2ceb827c52c3e
Muthurajchella- <muthuraj.chella@gmail.com>
2022-12-08 06:32:45 +0530

    portfolio

--- js/raj.js
+++ js/raj.js
@@ -1,36 +1,8 @@
 $(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
+    $('#icon').click(function(){
+        // $(this).toggleClass('bx bx-menu');
+        $('ul').toggleClass('show');
     });
-
-
-    $(window).on('scroll load', function(){
-        $('menu').romoveClass('fa-times');
-        $('header').removeClass('toggle')
-    });
-
-    //smoth scrolling
-
-    $('a[href*="#"]').on('click',function(e){
-        
-
-        e.prevenDefault();
-
-        $('html, body').animate({
-            scrollTop : $($(this).attr('href')).offset().top,
-        },
-          500,
-          'linear'
-            
-        );
-    })
 });
 
 
-//Date for Footer
-
-let date = new Date();
-
-let year = date.getFullYear();
-document.getElementById("date").innerHTML = year;
